872826cb0cf906692c91c9593288d6eb021c19b0
Kheoss <kheoss@localhost.localdomain>
2024-04-26 06:40:55 +0200

    Add sync

--- run_test.sh
+++ run_test.sh
@@ -7,10 +7,17 @@ fi
 
 num_instances=$1
 
-for ((i=1; i<=num_instances; i++))
-do
-  java -jar demo-jvm/build/libs/demo-jvm-all.jar  > /dev/null & 
-  #  ./gradlew :demo-jvm:run &
-done
+# for ((i=1; i<=num_instances; i++))
+# do
+#   java -jar demo-jvm/build/libs/demo-jvm-all.jar  & 
+#   #  ./gradlew :demo-jvm:run &
+# done
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+(java -jar demo-jvm/build/libs/demo-jvm-all.jar  ) > >(while read line; do echo -e "${RED}$line${NC}"; done) &
+
+(java -jar demo-jvm/build/libs/demo-jvm-all.jar  ) > >(while read line; do echo -e "${GREEN}$line${NC}"; done) &
 wait
